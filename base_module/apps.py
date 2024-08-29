ea05067934859c2180b517a98574edff440b24b7
moffatmore <mofenyimoffat@gmail.com>
2024-08-29 18:02:43 +0200

    refactor: 🎨 clean up base module

--- base_module/apps.py
+++ base_module/apps.py
@@ -1,6 +1,6 @@
 import sys
 
-from django.apps import AppConfig as DjangoAppConfig
+from django.apps import AppConfig as BaseAppConfig
 from django.conf import settings
 from django.core.management.color import color_style
 from django.core.exceptions import ImproperlyConfigured
@@ -8,13 +8,12 @@ from django.core.exceptions import ImproperlyConfigured
 style = color_style()
 
 
-class AppConfig(DjangoAppConfig):
-	name = 'base_module'
-	verbose_name = 'Base Module'
-	
-	def ready(self):
-		sys.stdout.write(
-			f' * default TIME_ZONE {settings.TIME_ZONE}.\n')
-		if not settings.USE_TZ:
-			raise ImproperlyConfigured('EDC requires settings.USE_TZ = True')
-		sys.stdout.write(f' Done loading {self.verbose_name}.\n')
+class AppConfig(BaseAppConfig):
+    name = "base_module"
+    verbose_name = "Base Module"
+
+    def ready(self):
+        sys.stdout.write(f" * default TIME_ZONE {settings.TIME_ZONE}.\n")
+        if not settings.USE_TZ:
+            raise ImproperlyConfigured("LOCHOIS requires settings.USE_TZ = True")
+        sys.stdout.write(f" Done loading {self.verbose_name}.\n")
