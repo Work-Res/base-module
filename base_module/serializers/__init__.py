0b35554247245569884785c3fc9ed12d473f6584
moffatmore <mofenyimoffat@gmail.com>
2024-09-11 12:01:15 +0200

    deleted spouce and child model serializers

--- base_module/serializers/__init__.py
+++ base_module/serializers/__init__.py
@@ -2,5 +2,3 @@
 # from .contact_serializer import ContactSerializer
 # from .passport_serializer import PassportSerializer
 # from .person_serializer import PersonSerializer
-from .spouse_serializer import SpouseSerializer
-from .child_serializer import ChildSerializer
