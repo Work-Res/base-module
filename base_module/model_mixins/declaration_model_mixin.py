bd05762c82e566ea7ab82b5dd0d63f28c7ff9632
moffatmore <mofenyimoffat@gmail.com>
2024-10-18 09:56:40 +0200

    Fixed declaration  model mixin, set fields to nullable

--- base_module/model_mixins/declaration_model_mixin.py
+++ base_module/model_mixins/declaration_model_mixin.py
@@ -4,18 +4,30 @@ from django.db import models
 class DeclarationModelMixin(models.Model):
 
     declaration_fname = models.CharField(
-        verbose_name="Declaration firstname", max_length=150
+        verbose_name="Declaration firstname",
+        max_length=150,
+        null=True,
+        blank=True,
     )
 
     declaration_lname = models.CharField(
-        verbose_name="Declaration lastname", max_length=150
+        verbose_name="Declaration lastname",
+        max_length=150,
+        null=True,
+        blank=True,
     )
 
     declaration_date = models.DateField(
         # validation=date_not_future
+        null=True,
+        blank=True,
     )
 
-    signature = models.CharField(max_length=190)
+    signature = models.CharField(
+        max_length=190,
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         abstract = True
