838e1cf27cf7a59f9dc410f15292e98919ceaa14
moffatmore <mofenyimoffat@gmail.com>
2024-10-18 09:47:14 +0200

    Fixed commissioner oath model mixin, set fields to nullable

--- base_module/model_mixins/commissioner_oath_model_mixin.py
+++ base_module/model_mixins/commissioner_oath_model_mixin.py
@@ -3,21 +3,41 @@ from django.db import models
 
 class CommissionerOathModelMixin(models.Model):
 
-    declaration_place = models.CharField(max_length=190)
+    declaration_place = models.CharField(
+        max_length=190,
+        null=True,
+        blank=True,
+    )
 
     oath_datetime = models.DateTimeField(
+        null=True,
+        blank=True,
         # validation=date_not_future,
         # auto_now_add=True
     )
 
-    commissioner_name = models.CharField(max_length=255)
+    commissioner_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
 
     # choice field?
-    commissioner_designation = models.CharField(max_length=255)
+    commissioner_designation = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
 
-    telephone_number = models.PositiveIntegerField()
+    telephone_number = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
 
-    commissioner_signature = models.TextField()
+    commissioner_signature = models.TextField(
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         abstract = True
